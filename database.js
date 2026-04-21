b02ddd4ca8ac5ffae385b4ac11ffc9ff187f8f15
Marilia-Savina <mariliasavina@gmail.com>
2026-04-21 19:01:39 -0300

    adicição dados no database.db

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose()
-const {open} = require ('sqlite')
+const { open } = require('sqlite')
 
 
 const banco = async () => {
@@ -23,7 +23,7 @@ const banco = async () => {
     `)
     console.log('tabela abrigo criada com sucesso!!!!')
 
-     await db.exec(`
+    await db.exec(`
        CREATE TABLE IF NOT EXISTS pedidos (
        id INTEGER PRIMARY KEY AUTOINCREMENT,
        nome TEXT,
@@ -34,6 +34,21 @@ const banco = async () => {
     `)
     console.log('tabela pedidos criada com sucesso!!!')
 
-return db
+
+    // dados para visualização
+
+    const primeirosDados = await db.get(`SELECT COUNT (*) AS Total FROM abrigos`)
+    if (primeirosDados.Total === 0) {
+        await db.exec(`
+            INSERT INTO abrigos (nome, endereco, telefone, vagas_total, vagas_disponiveis, aceita_pets, latitude, longitude)
+            VALUES
+            (' EEFM monte belo', 'rua 7 de setembro', '21 934265743', '35', '14', 'sim', '43.455', '54.987'),
+            ('quadra juventus sport','bom jardim,26','22 946734189','90','43','sim','21.343','13.432')
+            `)
+    } else {
+        console.log(` banco pronto com ${primeirosDados.Total} os primeiros dados`)
+    }
+
+    return db
 }
-module.exports = {banco}
+module.exports = { banco }
